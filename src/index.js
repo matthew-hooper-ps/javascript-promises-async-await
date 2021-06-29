601b4c6f1dd6274b46f175ca5ea964b970550d57
Matthew Hooper <matthew-hooper@pluralsight.com>
2021-06-28 20:20:19 -0600

    module 8.

--- src/index.js
+++ src/index.js
@@ -32,30 +32,26 @@ async function getBooksAndMoviesAsync() {
   }
   catch (error) {
     console.log("Error fetching books and movies", error)
+    return error
   }
 }
 
 async function getBooksOrMoviesAsync() {
-  try {
-    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()])
-    return values
-  }
-  catch (error) {
-    console.error("Error waiting for the promise race", error)
-  }
+  const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()])
+  return values
 }
 
-getBooksAndMoviesAsync()
-  .then(results => {
-    console.log("movies and books", {
-      movies: results.movies,
-      books: results.books
-    })
+getBooksAndMoviesAsync().then(results => {
+  console.log("movies and books", {
+    movies: results.movies,
+    books: results.books
   })
+}).catch(error => {
+  console.error("Error in getBooksAndMoviesAsync execution", error)
+})
 
-getBooksOrMoviesAsync()
-  .then(results => {
-    console.log("movies OR books", {
-      results
-    });
+getBooksOrMoviesAsync().then(results => {
+  console.log('movies OR books', {
+    results,
   })
+})
